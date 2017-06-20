94197fb11e479d5ca1dd842978719c666a7d364b
ianyudana <yudanaian@gmail.com>
2017-06-20 13:39:26 +0700

    Add files via upload

---
+++ UAS/question.js
@@ -0,0 +1,71 @@
+var questions = [{
+		"question": "Hasil dari 314.232 - 54.876 + 489.112 = ...",
+		"option1": "748.486",
+		"option2": "748.468",
+		"option3": "746.468",
+		"option4": "746.486",
+		"answer": "2"
+}, {
+		"question": "Hasil 435 + (-325) : (-13) - 422 = ...",
+		"option1": "38",
+		"option2": "130",
+		"option3": "83",
+		"option4": "30",
+		"answer": "1"
+}, {
+		"question": "Hasil dari 1.566 + 960 : 20 =...",
+		"option1": "1.146",
+		"option2": "1.164",
+		"option3": "1.416",
+		"option4": "1.614",
+		"answer": "4"
+}, {
+		"question": "Hasil dari operasi hitung 22.135 : 5 x 43 =...",
+		"option1": "190.631",
+		"option2": "190.163",
+		"option3": "190.361",
+		"option4": "190.316",
+		"answer": "3"
+}, {
+		"question": "Suhu udara di kota Bangkok saat pagi adalah -3'C. Saat siang, suhu tersebut naik sebanyak 14'C. Lalu saat malam suhunya naik lagi 16'C. Maka suhu udara di Bangkok saat malam adalah... ",
+		"option1": "4",
+		"option2": "-5",
+		"option3": "-4",
+		"option4": "5",
+		"answer": "2"
+}, {
+		"question": "Di toko X ada 17 karung beras yang masing-masing berisi 25 kg. Hari ini toko X mendapat tambahan kiriman tepung sebanyak 132 Kg. berapa berat total tepung saat ini di toko X ?",
+		"option1": "556",
+		"option2": "557",
+		"option3": "558",
+		"option4": "559",
+		"answer": "2"
+}, {
+		"question": "145 : (-5) + 23 - 85 =...",
+		"option1": "91",
+		"option2": "-77",
+		"option3": "77",
+		"option4": "-91",
+		"answer": "4"
+}, {
+		"question": "Hasil dari -134 x 5 + 46 - (-98) adalah...",
+		"option1": "-632",
+		"option2": "-624",
+		"option3": "526",
+		"option4": "722",
+		"answer": "3"
+}, {
+		"question": "Hasil dari 435 + (-525) : (-15) - 676 adalah..",
+		"option1": "-669",
+		"option2": "-206",
+		"option3": "-682",
+		"option4": "-1.146",
+		"answer": "2"
+}, {
+		"question": "Hasil dari -25 + 15 x 5 - 25 adalah...",
+		"option1": "20",
+		"option2": "25",
+		"option3": "-20",
+		"option4": "-25",
+		"answer": "2"
+}]
